f5eea8fbe7bfcb758d3b5814fd69d19f3e98f46c
kewilson <kewilson@users.noreply.github.com>
2018-12-11 22:40:10 -0600

    splash and icon tweaking.

--- App.js
+++ App.js
@@ -1,53 +1,50 @@
 import React from 'react';
-//import {AppLoading, Asset, Font, Icon} from 'expo';
+import {AppLoading, Asset} from 'expo';
 import Doctors from './app/Doctors'
-
 
 export default class App extends React.Component {
 
-    // state = {
-    //     isLoadingComplete: false
-    // };
+    state = {
+        isLoadingComplete: false
+    };
 
 
     render() {
-        // if (!this.state.isLoadingComplete) {
-        //     return (
-        //         <AppLoading
-        //             startAsync={this._loadResourcesAsync}
-        //             onError={this._handleLoadingError}
-        //             onFinish={this._handleFinishLoading}
-        //         />
-        //     );
-        // } else {
+        if (!this.state.isLoadingComplete) {
             return (
-                //<View><Doctors/></View>
+                <AppLoading
+                    startAsync={this._loadResourcesAsync}
+                    onError={this._handleLoadingError}
+                    onFinish={this._handleFinishLoading}
+                />
+            );
+        } else {
+            return (
                 <Doctors/>
             );
-        //}
+        }
     }
 
-    // _loadResourcesAsync = async () => {
-    //     return Promise.all([
-    //         Asset.loadAsync([
-    //             require('./assets/images/robot-dev.png'),
-    //             require('./assets/images/robot-prod.png'),
-    //         ]),
-    //         Font.loadAsync({
-    //             ...Icon.Ionicons.font,
-    //             'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    //         }),
-    //     ]);
-    // };
-    //
-    // _handleLoadingError = error => {
-    //     // In this case, you might want to report the error to your error
-    //     // reporting service, for example Sentry
-    //     console.warn(error);
-    // };
-    //
-    // _handleFinishLoading = () => {
-    //     this.setState({isLoadingComplete: true});
-    // };
+    _loadResourcesAsync = async () => {
+        return Promise.all([
+            Asset.loadAsync([
+                require('./assets/images/robot-dev.png'),
+                require('./assets/images/robot-prod.png'),
+            ]),
+            // Font.loadAsync({
+            //     ...Icon.Ionicons.font,
+            //     'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+            // }),
+        ]);
+    };
 
+    _handleLoadingError = error => {
+        // In this case, you might want to report the error to your error
+        // reporting service, for example Sentry
+        console.warn(error);
+    };
+
+    _handleFinishLoading = () => {
+        this.setState({isLoadingComplete: true});
+    };
 }
